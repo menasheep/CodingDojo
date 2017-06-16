14cec38954cf3978a60e55c2b56415dcfb8c3801
Malina Morning <menasheep@gmail.com>
2017-06-16 11:20:17 -0700

    Add files via upload

---
+++ C-sharp/Projects/dojo_survey/dojo_survey/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.IO; // contains all classes that deal w/ input/output
+using Microsoft.AspNetCore.Hosting;
+
+namespace dojo_survey{
+    public class Program{
+        public static void Main(string[] args){
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+}
